94344163d71b456e46775720b4d6f88bb2938cb3
flolilo <30194876+flolilo@users.noreply.github.com>
2020-01-22 17:58:50 +0100

    proof of concept for calling ffmpeg and files

--- yabg.py
+++ yabg.py
@@ -4,3 +4,19 @@
 
 import os
 import sys
+import subprocess
+import shlex
+# binary = input("Provide binary (including path): ")
+binary = "/bin/ffmpeg"
+# arguments = input("Provide arguments for that binary: ")
+arguments = "-hide_banner -i %file% -c:v libx265 -preset veryslow -crf 30 -y test.mkv"
+filelist = ["/path/to/file1", "/path/to/file2"]
+
+print(binary)
+print(arguments)
+for i in filelist:
+    x = str(binary + " " + str(arguments.replace("%file%", str('"' + str(i) + '"'))))
+    x = shlex.split(x)
+    print(x)
+    subprocess.run(x)
+    # TODO: https://stackoverflow.com/questions/10965949
